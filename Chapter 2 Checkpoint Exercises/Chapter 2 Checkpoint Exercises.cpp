ea4744786e427af25e2ce5cde92cb14bfce10b40
Drasc001 <dtr2170@email.vccs.edu>
2025-03-12 11:51:35 -0400

    Checkpoint_2-20

--- Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
+++ Chapter 2 Checkpoint Exercises/Chapter 2 Checkpoint Exercises.cpp
@@ -1,11 +1,15 @@
-// Chapter 2 Checkpoint Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This program defines an integer variable 'age' and a double variable 'weight',
+// stores literal values for age and weight in these variables, and displays them on the screen.
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int age = 20;
+    double weight = 195;
+    cout << "My age is " << age << " and my weight is " << weight << endl;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
